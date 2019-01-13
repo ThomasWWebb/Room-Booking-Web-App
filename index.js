6dea076e90a7d11111e398b4556ca1d66a0d4df7
ThomasWWebb <thomaswebb98uk@gmail.com>
2019-01-13 21:34:26 +0000

    signs up a new user without a password from the overlay

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 var express = require('express');
 var app = express();
+var fs = require('fs');
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'));
